a58c4cff2f2134d8064a2c20ed6f301d6d44ea0e
Rafiester <rafi.haqqi@gmail.com>
2024-01-17 01:51:43 +0700

    sweet: Evolution X Bringup
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- evolution_sweet.mk
+++ evolution_sweet.mk
@@ -11,13 +11,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit common Evolution stuff.
+$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_sweet
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := evolution_sweet
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Bootanimation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+# Quick Tap
+TARGET_SUPPORTS_QUICK_TAP := true

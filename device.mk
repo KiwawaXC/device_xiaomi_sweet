f5d45b2b25933198612c659edc1dc2b542c629fa
Rafiester <rafi.haqqi@gmail.com>
2024-02-26 17:41:29 +0000

    Revert: "sweet: Enable casefolding support on userdata"
    Signed-off-by: Rafiester <rafi.haqqi@gmail.com>

--- device.mk
+++ device.mk
@@ -20,9 +20,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Enable project quotas and casefolding for emulated storage without sdcardfs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # Call the MiuiCamera setup
 $(call inherit-product-if-exists, vendor/xiaomi/sweet-miuicamera/products/miuicamera.mk)
 
